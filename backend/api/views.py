c96328aaacca9576761e885a54e842786b5a6408
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-03-29 17:50:58 -0500

    recheck views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, JournalEntrySerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import JournalEntry
 # Create your views here.
+
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class JournalEntryListView(generics.ListCreateAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return JournalEntry.objects.filter(user=user)
+
+
+class JournalEntryDetailView(generics.DestroyAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return JournalEntry.objects.filter(user=user)
