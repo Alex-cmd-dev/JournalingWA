e8a87bf5e7c6ecdc24c3a1799383277eb5f8d645
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-04-03 18:53:04 -0500

    working on consumers.py

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -8,3 +8,17 @@ from rest_framework.request import Request
 from .models import ChatMessage
 from .serializers import ChatMessageSerializer
 from asgiref.sync import async_to_sync
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.chatID = self.scope["url_route"]["kwargs"]["chatID"]
+        self.user = self.scope["user"]
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.chatID,
+            self.channel_name,
+        )
+
+
+
