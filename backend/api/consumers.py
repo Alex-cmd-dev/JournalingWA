5ecca1bef7a2988a138228b4e0d9e3d78819731f
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-04-04 14:34:17 -0500

    Understanding web sockets

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -9,16 +9,28 @@ from .models import ChatMessage
 from .serializers import ChatMessageSerializer
 from asgiref.sync import async_to_sync
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chatID = self.scope["url_route"]["kwargs"]["chatID"]
         self.user = self.scope["user"]
+        # Check if user is authenticated
+        if self.user.is_anonymous:
+            # Reject the connection
+            await self.close(code=4003)
+            return
 
         # Join room group
         await self.channel_layer.group_add(
             self.chatID,
             self.channel_name,
         )
+        # Accept the connection
+        await self.accept()
 
-
-
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.chatID,
+            self.channel_name,
+        )
